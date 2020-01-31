012a0e0f56a585ce33e7c58a037129239d129faa
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-01 01:19:23 +0700

    add validation error

--- WonderAPI/Pkg/AppException.cs
+++ WonderAPI/Pkg/AppException.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace WonderAPI.Pkg
 {
+    /// <summary>
+    /// Represent error per field.
+    /// </summary>
+    public class FieldError
+    {
+        public string Field { get; set; }
+        public string ErrorMessage { get; set; }
+        public List<FieldError> SubErrors { get; set; } = new List<FieldError>(); 
+    }
+
+    /// <summary>
+    /// Represent validation result, containing list of errors. When model validation failed, server will response with this model.
+    /// </summary>
+    public class ValidationResult : FieldError
+    {
+        public List<FieldError> Errors { get; set; } = new List<FieldError>();
+    }
+
+    /// <summary>
+    /// Represent the known error/exception happened in this application
+    /// </summary>
     public class AppException : Exception
     {
+        public ValidationResult ValidationResult { get; set; }
+
         public AppException()
         {
+        }
+
+        public AppException(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
         }
 
         public AppException(string message) : base(message)
