c9398a7ed1515761dd2043076097fed37db5d242
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-01 02:54:07 +0700

    add migration files

---
+++ WonderAPI/Migrations/20200131195225_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WonderAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    OptionalEmail = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: false),
+                    MobileNumber = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Member");
+        }
+    }
+}
