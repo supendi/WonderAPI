f29dcf8337667d6cd390c71e3b56ae2d25fd0c98
Irpan Supendi <irpan.supendi@gmail.com>
2020-01-31 20:24:24 +0700

    implement jwt generator

---
+++ WonderAPI/Controllers/Account/JWTGenerator.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WonderAPI.Entities;
+
+namespace WonderAPI.Controllers.Account
+{
+    public interface ITokenGenerator
+    {
+        string Generate(Member member);
+    }
+
+    public class JWTGenerator : ITokenGenerator
+    {
+        static string secretKey = "";
+
+        /// <summary>
+        /// Returns secret key from env. If null return default key;
+        /// </summary>
+        /// <returns></returns>
+        private static string GetSecretKey()
+        {
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                var jwtSecret = System.Environment.GetEnvironmentVariable("JwtSecret");
+                secretKey = jwtSecret;
+                if (secretKey == null)
+                    secretKey = "WonderLabs";
+            }
+
+            return secretKey;
+        }
+
+        /// <summary>
+        /// Generate JWT string
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        public string Generate(Member member)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(GetSecretKey());
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("name", member.Name),
+                    new Claim("dateOfBirth", member.DateOfBirth.ToString()),
+                    new Claim("gender", member.Gender),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return tokenString;
+        }
+    }
+}
--- WonderAPI/Controllers/Account/MemberController.cs
+++ WonderAPI/Controllers/Account/MemberController.cs
@@ -12,7 +12,7 @@ namespace WonderAPI.Controllers.Account
         [Route("{memberId}")]
         public MemberInfo GetMemberInfo([FromRoute]int memberId)
         {
-            using (var svc = new MemberService(new MemberRepository(new WonderDBContext()), new Pbkdf2Hasher(), new TokenGenerator()))
+            using (var svc = new MemberService(new MemberRepository(new WonderDBContext()), new Pbkdf2Hasher(), new JWTGenerator()))
             {
                 return svc.GetMemberInfo(memberId);
             }
@@ -21,7 +21,7 @@ namespace WonderAPI.Controllers.Account
         [HttpPost] 
         public Member CreatMember([FromBody]Member member)
         {
-            using (var svc = new MemberService(new MemberRepository(new WonderDBContext()), new Pbkdf2Hasher(), new TokenGenerator()))
+            using (var svc = new MemberService(new MemberRepository(new WonderDBContext()), new Pbkdf2Hasher(), new JWTGenerator()))
             {
                 return svc.RegisterNewMember(member);
             }
