dd289a7def5f60eccc8e602dc21076ede3f0955e
Irpan Supendi <irpan.supendi@gmail.com>
2020-01-31 14:30:54 +0700

    clean up and move model to pkg

--- WonderAPI/Pkg/Model/Member.cs
+++ WonderAPI/Pkg/Model/Member.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WonderAPI.Controllers.Account
+namespace WonderAPI.Pkg.Model
 {
     /// <summary>
     /// Represent Member business entity
---
+++ WonderAPI/Pkg/WonderDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WonderAPI.Pkg.Model;
+
+namespace WonderAPI.Pkg
+{
+    public class WonderDBContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+    }
+}
