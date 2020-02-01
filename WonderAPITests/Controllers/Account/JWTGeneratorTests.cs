cf3913babfa0fe41f77528f5272eab8d5dc4ccdf
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-01 17:48:16 +0700

    add unit test projects

---
+++ WonderAPITests/Controllers/Account/JWTGeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WonderAPI.Controllers.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+namespace WonderAPI.Controllers.Account.Tests
+{
+    [TestClass()]
+    public class JWTGeneratorTests
+    {
+        [TestMethod()]
+        public void GenerateTest()
+        {
+            var generator = new JWTGenerator();
+            var token = generator.Generate(new Entities.Member()
+            {
+                ID = 1,
+                Name = "John Doe",
+                Email = "john.doe@email.com",
+                OptionalEmail = "",
+                DateOfBirth = DateTime.Now,
+                Gender = "Male",
+                MobileNumber = "",
+                Password = "ASuperDuperStrongPasswordThatNoOneCanHack.YesItUseADot."
+            });
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+            var x = jwtToken.Claims.Count();
+            Assert.IsTrue(jwtToken.Claims.Count() == 6);
+        }
+
+        [TestMethod()]
+        public void GetSecretKeyTest()
+        {
+            var key = JWTGenerator.GetSecretKey();
+            Assert.AreEqual(key, "akusayangkamuselamanyah");
+        }
+    }
+}
