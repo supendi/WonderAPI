659259d3ae1e867e0baad1bfba7e54077f6fff55
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-01 20:21:07 +0700

    hash password test

---
+++ WonderAPITests/Controllers/Account/BCryptHasherTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WonderAPI.Controllers.Account.Tests
+{
+    [TestClass()]
+    public class BCryptHasherTests
+    {
+        [TestMethod()]
+        public void HashTest()
+        {
+            var hasher = new BCryptHasher();
+
+            var password = "aW3akp455w0rD";
+            var hashedPassword = hasher.Hash(password);
+
+            Assert.IsTrue(hasher.Verify(password, hashedPassword));
+        }
+    }
+}
